48b086d4036406b04cb2b1d643792d02f4709114
l0lman01 <63782031+l0lman01@users.noreply.github.com>
2022-11-14 13:02:34 +0100

    notebook nexplys

--- PythonApplication/PythonApplication.py
+++ PythonApplication/PythonApplication.py
@@ -1,1 +1,37 @@
+import sklearn
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import ElasticNet
+from sklearn.svm import SVR
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.linear_model import SGDRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.neural_network import MLPRegressor
+
+import numpy as np
+import scipy as sp
+import pandas as pd
+import matplotlib.pyplot as plt
+#import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import r2_score
+
+#dataset = fetch_california_housing(data_home=None, as_frame=True)
+#print(dataset)
+
+df = pd.DataFrame(
+    {
+        "Name": [
+            "Braund, Mr. Owen Harris",
+            "Allen, Mr. William Henry",
+            "Bonnell, Miss. Elizabeth",
+        ],
+        "Age": [22, 35, 58],
+        "Sex": ["male", "male", "female"],
+    }
+)
+#print(df.describe)
 
